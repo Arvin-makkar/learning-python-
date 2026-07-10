6a59faf6d51a12523726d45c75f1dd75343f5d9a
Arvin Makkar <arvinkumarm327@gmail.com>
2026-07-10 21:58:04 +0530

    Add various calculations and user inputs in practice.py

--- practice.py
+++ practice.py
@@ -16,3 +16,84 @@ print('D  D  L')
 print('D  D  L')
 print('DDDD  L')
 print('Congrats ! you\'ve made it to the end of chapter one!')
+name = 'Erlich Bachman'
+user_id = 16180339887
+progress = 0.75
+xp = 60 
+verified = true
+print(name)
+print(user_id)
+print(progress)
+print(xp)
+print(verified)
+score = 0 
+score = 4 + 3
+score = 4 - 3
+score = 4 * 3
+score = 4 / 3
+print(score)
+pizza = 2.99
+coke = 0.99
+total = pizza + coke 
+tip = total * 0.2 
+print(tip)
+temp_f = 76
+temp_c = (temp_f - 32)/1.8
+print(temp_c)
+score = 5 % 3
+print(score)
+score = 5 % 2
+print(score)
+score = 5 % 1 
+print(score)
+score = 2 ** 2 
+print(score)
+score = 2 ** 3
+print(score)
+score = 2 ** 4 
+print(score)
+mass = 16 
+height = 14
+bmi = mass/height**2
+print(bmi)
+username = input('Arvin')
+print(username)
+age = int(input('18'))
+print(age)
+import math 
+a = 12
+b = 13 
+c = 15
+#calculate hypotenuse 
+c = math.sqrt(a**2 + b**2)
+print(c)
+pesos = float(input("what do you have left in pesos? "))
+soles = float(input("what do you have left in soles? "))
+reais = float(input("what do you have left in reais? "))
+usd = (pesos/56) + (soles/3.5) + (reais/5)
+print(usd)
+year = 2028
+focus = 0.6
+skin = 'My Mistake'
+shedding = True 
+print(year)
+print(focus)
+print(skin)
+print(shedding)
+balance = 480 
+rate = 0.08
+balance = balance +(balance * rate)
+print(balance)
+transistors = 1E+13
+years = 26
+transistors = transistors* 2**(year/2)
+print(transistors)
+num=8
+print(num % 2)
+num = 7
+print(num % 2)
+age = int(input"enter you dog's age: "))
+human_age = age * 7 
+print(human_age)
+
+    
