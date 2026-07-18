7a9db99d0a89c318306ed59563917e0de1762e97
Arvin Makkar <arvinkumarm327@gmail.com>
2026-07-18 10:34:09 +0530

    Update practice.py

--- practice.py
+++ practice.py
@@ -438,3 +438,43 @@ things_to_do = ["travel to japan",
 print(things_to_do)
 
 
+# This was the first function introduced in the course
+print('Hello World!')
+
+# This function calculates the maximum of two numbers
+print(max(3, 5))
+
+# type() - shows the data type of a value
+print(type("25"))
+
+
+import random
+
+options = [
+  'Don’t pursue happiness – create it.',
+  'All things are difficult before they are easy.',
+  'The early bird gets the worm, but the second mouse gets the cheese.',
+  'If you eat something and nobody sees you eat it, it has no calories.',
+  'Someone in your life needs a letter from you.',
+  'Don’t just think. Act!',
+  'Your heart will skip a beat.',
+  'The fortune you search for is in another cookie.',
+  'Help! I’m being held prisoner in a Chinese bakery!'
+]
+
+def fortune():
+  random_fortune = random.randint(0, len(options) - 1)
+  print(options[random_fortune])
+
+fortune()
+fortune()
+fortune()
+
+
+def distance_to_miles(distance):
+    miles = distance / 1.609
+    print(miles)
+
+distance_to_miles(10000)
+
+
